418aab12949791bca05d1e8b4fddd0199f349425
Bill Avery <billav@microsoft.com>
2024-08-27 14:58:53 -0700

    fix: update relative paths in mapi-win32md to match new repo

--- mapi-win32md/update.ps1
+++ mapi-win32md/update.ps1
@@ -1,5 +1,3 @@
-param($mapi_rs_path)
-
 & dotnet build
 
-Copy-Item .\bin\Microsoft.Office.Outlook.MAPI.Win32.winmd "$mapi_rs_path\crates\update-bindings\winmd\"
+Copy-Item .\bin\Microsoft.Office.Outlook.MAPI.Win32.winmd ..\crates\update-bindings\winmd\
